1bd669981e4f0459b20ba89b16504d1141e40483
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-04-25 13:59:10 +0300

    Create doolittle.m

---
+++ System of Linear Equations/LU Factorization/doolittle.m
@@ -0,0 +1,21 @@
+% Factorizare Doolittle
+% L = matrice inferior triunghiulara
+% U = matrice superior triunghiulara
+
+function [L U] = doolittle(A)
+  [n n] = size(A);
+  L = eye(n);
+  U = zeros(n);
+  
+  for p = 1:n
+    for j = p:n
+      s = L(p, 1:p-1)*U(1:p-1, j);
+      U(p, j) = A(p, j) - s;
+    endfor
+    
+    for i = p+1:n
+      s = L(i, 1:p-1)*U(1:p-1, p); 
+      L(i, p) = (A(i, p) - s)/U(p, p);
+    endfor
+  endfor
+endfunction
