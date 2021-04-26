06baca62cf8eefca91a5f0aac5fa59356e14e42b
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-04-26 16:59:56 +0300

    Update CroutSDG.m

--- System of Linear Equations/LU Factorization/CroutSDG.m
+++ System of Linear Equations/LU Factorization/CroutSDG.m
@@ -1,1 +1,26 @@
-% Metoda Crout adaptata pentru matrice simetrica tridiagonalafunction [L U] = CroutSDG(A)  [n n] = size(A);  L = zeros(n);  U = zeros(n);    v = A(1,1);  u = A(2,1)/v;    L(1,1) = v;  U(1,1) = 1;  U(1,2) = u;    for i=2:n    v = A(i,i) - A(i,i-1)*u;    if i!=n      u = A(i+1,i)/v;      U(i,i+1) = u;    endif        U(i,i)=1;    L(i,i)=v;    L(i,i-1)=A(i,i-1);  endforendfunction
+% Metoda Crout adaptata pentru matrice simetrica tridiagonala
+function [L U] = CroutSDG(A)
+  [n n] = size(A);
+  L = zeros(n);
+  U = zeros(n);
+  
+  v = A(1,1);
+  u = A(2,1)/v;
+  
+  L(1,1) = v;
+  U(1,1) = 1;
+  U(1,2) = u;
+  
+  for i=2:n
+    v = A(i,i) - A(i,i-1)*u;
+    if i!=n
+      u = A(i+1,i)/v;
+      U(i,i+1) = u;
+    endif
+    
+    U(i,i)=1;
+    L(i,i)=v;
+    L(i,i-1)=A(i,i-1);
+  endfor
+
+endfunction
