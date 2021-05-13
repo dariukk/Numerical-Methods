d6eb566cac4ca431e59fe5390d220ce1bc26474f
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-05-13 20:09:25 +0300

    Update bilinear_rotate_RGB.m

--- homework/bilinear/bilinear_rotate_RGB.m
+++ homework/bilinear/bilinear_rotate_RGB.m
@@ -4,14 +4,24 @@ function out = bilinear_rotate_RGB(img, rotation_angle)
     % =========================================================================
     
     % TODO: extrage canalul rosu al imaginii
+    red = img(:, :, 1);
 
     % TODO: extrage canalul verde al imaginii
-
+    green = img(:, :, 2);
+    
     % TODO: extrace canalul albastru al imaginii
+    blue = img(:, :, 3); 
 
-
-    % TODO: aplică rotația pe fiecare canal al imaginii
-
-    % TODO: reconstruiește imaginea RGB finala (hint: cat)
+     % TODO: aplica functia nn pe cele 3 canale ale imaginii
+    red_ref = bilinear_rotate(red, rotation_angle);
+    green_ref = bilinear_rotate(green, rotation_angle);
+    blue_ref = bilinear_rotate(blue, rotation_angle);
+    
+    % TODO: formeaza imaginea finala cu cele 3 canale de culori
+    % Hint: functia cat
+    
+    out(:,:,1) = red_ref;
+    out(:,:,2) = green_ref;
+    out(:,:,3) = blue_ref;
     
 endfunction
